a648a27c010474d320e9d6c00fa7c162b8ab7d35
Paul Logston <paul@Logstons-MacBook-Pro.local>
2021-01-03 22:37:16 -0800

    Move to GNU Stow

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
     curl -s https://raw.githubusercontent.com/logston/dotfiles/master/install.sh | bash
 
 Set the command for terminal to run upon opening to: `/usr/local/bin/tmux attach-session`
+
+Make sure GUN Stow is installed
+
+	brew install stow
--- install.sh
+++ install.sh
@@ -4,30 +4,18 @@ if [ ! -d ~/.dotfiles ]; then
     git clone git@github.com:logston/dotfiles.git ~/.dotfiles
 fi
 
+if ! command -v stow &> /dev/null
+then
+    brew install stow
+fi
+
 cd ~/.dotfiles
 
-for file in .*;
-do
-	if [ ! -f "$file" ]; then
-		continue
-	fi
-
-	if [ "$file" == '.ctags-index' ]; then
-		continue
-	fi
-
-	if [ "$file" == '.gitignore' ]; then
-		continue
-	fi
-
-	if [ "$file" == '.' ] || [ "$file" == '..' ]; then 
-		continue
-	fi
-
-	rm ~/$file
-	ln -s $(pwd)/$file ~/$file
-
-	echo "Placed $(pwd)/$file -> ~/$file"
-done
-
-cd -
+stow bash
+stow ctags
+stow git
+stow less
+stow pdbpp
+stow tmux
+stow vim
+stow zsh
